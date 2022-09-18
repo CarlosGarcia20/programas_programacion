3cb862b18a8852856f417cf31ff4f63e35b143c3
Carlos Garcia <111941326+CarlosGarcia20@users.noreply.github.com>
2022-09-18 14:02:49 -0600

    otra parte de la presentacion

---
+++ declaracion_a_plazo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int add(int x, int y);
+
+int main()
+{
+	std::cout<<"La suma de 3 y 4 es: "<<add(3, 4)<<'\n';
+	return 0;
+}
+
+int add(int x, int y)
+{
+	return x+y;
+}
+
+
