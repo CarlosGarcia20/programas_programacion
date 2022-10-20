7a51023796d36954391fdb6b62da00ab102f9b27
Carlos Garcia <111941326+CarlosGarcia20@users.noreply.github.com>
2022-10-20 11:22:34 -0600

    Segunda parte del proyecto
    En esta segunda parte e agregado la parte de la venta de productos. Se pregunta el ID del producto, cuanto va a comprar y el precio final, al final pregunta si desea hacer otra compra. Hay un acumulador que va mostrando el precio final. Me falta corregir que se vaya degradando el stock disponible

--- Punto de Venta/Punto de venta.cpp
+++ Punto de Venta/Punto de venta.cpp
@@ -4,8 +4,6 @@
 
 using namespace std;
 
-int opc, op, op1, p;
-
 //Orden: id, descripcion, precio de venta, cantidad de producto, stock minimo
 string productos[20][4];
 //Orden: id, nombre, credito, adeudo
@@ -14,6 +12,20 @@ string clientes[5][4];
 string ventas[50][4];
 //Total de toda la venta
 string ventast[5][4];
+
+int opc, opc1=1, resp, op, op1, i;
+float acum;
+
+int menu(void)
+{
+	cout<<"-- P U N T O  D E  V E N T A  C A R L O S  G A R C I A --"<<'\n';
+	cout<<"[1] Vender"<<'\n';
+	cout<<"[2] Productos"<<'\n';
+	cout<<"[3] Clientes"<<'\n';
+	cout<<"[4] Registros de venta "<<'\n';
+	cout<<"[5] Ventas totales"<<'\n';
+	cout<<"[6] Productos en stock"<<'\n';
+}
 
 int main()
 {
@@ -24,38 +36,39 @@ int main()
 	
 	//Llenado de la matriz de productos
 	productos[0][0]="0";productos[0][1]="Takis";productos[0][2]="15";productos[0][3]="20";productos[0][4]="10";
-	productos[1][0]="1";productos[1][1]="Arizona";productos[0][2]="18";productos[0][3]="12";productos[0][4]="6";
+	productos[1][0]="1";productos[1][1]="Arizona";productos[1][2]="18";productos[1][3]="12";productos[1][4]="6";
 	
-	cout<<"-- P U N T O  D E  V E N T A  C A R L O S  G A R C I A --"<<'\n';
-	cout<<"[1] Vender"<<'\n';
-	cout<<"[2] Productos"<<'\n';
-	cout<<"[3] Clientes"<<'\n';
-	cout<<"[4] Registros de venta "<<'\n';
-	cout<<"[5] Ventas totales"<<'\n';
-	cout<<"[6] Productos en stock"<<'\n';
+	menu();
 	cin>>opc;
 	switch (opc)
 	{
 		case 1:
 			{
-				system("cls");
-				cout<<"Seleccione el producto a vender: "<<'\n';
-				cin>>op;
-				cout<<"Cantidad de "<<productos[p][1]<<" que hay disponible: "<<productos[p][2]<<'\n';
-				cout<<"Cantidad de productos a comprar: "<<'\n';
-				cin>>op1;
-				
-				if (op1<0)
+				do
 				{
-					cout<<"Ingrese una cantidad correcta";
-				}
-				
-				
-				
-				
-				
-				
-				
+					system("cls");
+					cout<<"Seleccione el producto a vender: "<<'\n';
+					cin>>op;
+					cout<<"Cantidad de "<<productos[op][1]<<" que hay disponible: "<<productos[op][3]<<'\n';
+					int f;
+					f=stof(productos[op][2]);
+					cout<<"Cantidad de productos a comprar: "<<'\n';
+					cin>>op1;
+					if (op1<0)
+					{
+						cout<<"Ingrese una cantidad correcta";
+					}
+					else if (op1>f)
+					{
+						cout<<"Ingrese una cantidad menor";
+					}
+					int cant;
+					cant=op1*f;
+					acum=acum+cant;
+					cout<<"El precio final es de: "<<acum<<'\n';
+					cout<<"Desea hacer otra compra? 1-Si -- 2-No"<<'\n';
+					cin>>resp;
+				} while (opc1==resp);
 				
 				break;
 			}
